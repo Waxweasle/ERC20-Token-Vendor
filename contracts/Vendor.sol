465968723c6c1b5b631a14c80429dd1628e4481c
Matthew <103790652+Waxweasle@users.noreply.github.com>
2022-08-05 17:48:44 -0400

    Update Vendor.sol

--- contracts/Vendor.sol
+++ contracts/Vendor.sol
@@ -3,8 +3,9 @@ pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Vendor is Ownable, gpToken {
+contract Vendor is Ownable {
     event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
     YourToken public yourToken;
@@ -15,7 +16,14 @@ contract Vendor is Ownable, gpToken {
 
     uint256 public constant tokensPerEth = 100;
 
+    // ToDo: create a payable buyTokens() function:
     function buyTokens() public payable {
         uint256 amountOfTokens = tokensPerEth * msg.value;
         emit BuyTokens(msg.sender, msg.value, amountOfTokens);
     }
+
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw(uint _amount) public onlyOwner {
+        payable(msg.sender).transfer(_amount);
+    }
+}
